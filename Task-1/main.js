7f11d8062c7b7b984cfb1c5576b24a0aa77e7a7c
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-10 12:05:10 +0500

    commit

--- Task-1/main.js
+++ Task-1/main.js
@@ -9,3 +9,24 @@
 
 
 // console.log(kvadrat(5));
+
+//   let rang1 = "oq"
+//   let rang2 = "qora"
+
+// const person = {
+//     iam:12,
+//     id :'kim'
+// }
+ 
+// for(let key in person ){
+//     // if(key = 12){
+//     //     console.log(key);
+        
+//     // }
+//     console.log(key, person[key]);
+
+// }
+
+
+
+ 
