cab733c7a475be0f7c7795c27eeea7bc095c7d0b
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-10 23:22:39 +0500

    commit

--- Task-1/main.js
+++ Task-1/main.js
@@ -1,12 +1,10 @@
 // let ranglar = ["oq",'qizl']
 // console.log(ranglar.length);
 
-
 // function kvadrat (num) {
-   
+
 //     return   num * num
 // }
-
 
 // console.log(kvadrat(5));
 
@@ -17,30 +15,55 @@
 //     iam:12,
 //     id :'kim'
 // }
- 
+
 // for(let key in person ){
 //     // if(key = 12){
 //     //     console.log(key);
-        
+
 //     // }
 //     console.log(key, person[key]);
 
 // }
 
-
 // let ranglar = [ 'oq','qora','pushti']
 // for(let index in ranglar){
 //     console.log(index, ranglar[index]);
-// } 
-
+// }
 
 // let sonlar = (son1,son2) => {
 //     if (son1< son2) {
 //        return son2
-        
+
 //     }else{
 //        return son1
 //     }
 // }
 // console.log(sonlar(2,3));
 
+// let rasm = (boyi, eni) => {
+//     return (boyi<eni) ? true : false
+//           yokida
+//     return (boyi<eni)
+// console.log(rasm(200,300));
+
+// let fizzBuzz = (son) => {
+//   if (typeof son !== "Number") {
+//     if (son % 3 == 0 && son % 5 == 0) {
+//         return "fizzBuzz";
+//       }
+
+//     if (son % 3 == 0) {
+//       return "fizz";
+//     }
+//     if (son % 5 == 0) {
+//       return "buzz";
+//     }
+   
+//     if (son % 3 == 0 && son % 5 !== 0) {
+//       return son;
+//     }
+//   } else {
+//     return "son emas";
+//   }
+// };
+// console.log(fizzBuzz(16));
