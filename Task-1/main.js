2e9f736f5f9d3ff75da079bfeabd5e74a8e6548d
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-11 11:20:38 +0500

    string

--- Task-1/main.js
+++ Task-1/main.js
@@ -67,3 +67,61 @@
 //   }
 // };
 // console.log(fizzBuzz(16));
+
+
+let str = "   alakimfalonchiyev2@gmao.com  "
+// topilgan birinchisini oladi
+
+// let natija = str.indexOf('a')
+
+// topilgan oxiridagini oladi 
+
+// let natija = str.lastIndexOf('a')
+
+// berilgan indexlardan berilgan oraligini oladi 
+// let natija = str.slice(1,3)
+
+// berilga indexdan keyingisini oladi 
+// let natija = str.slice(3)
+
+// birinch index start bo'ladi ikkinchi son tugash joyi 
+// birinchi kirtilgan sondan ikkinchi songacha olib beradi
+// let natija = str.substring(1,2)
+
+// stringni almashtirish birinchi kiritilgan harfni harfni ikkinchi kirtilgan qiymatga almashtiradi
+// faqat birinchi topganiga
+// let natija = str.replace("a","2")
+
+// kirtilgan indexdagi harfni olib beradi
+// let natija = str.charAt(1)
+
+// boshidagi va oxridagi bo'sh joylarni olib tashlaydi 
+// let natija = str.trim()
+
+// stringni array qilib beradi
+// let natija = str.split()
+
+
+// let natija = str.split(" ")
+// agar split(" ") bo'sh joy qilinsa bo'sh joylarni alohida array qilib beradi 
+
+
+// agar split('') bo'sh joy qoldirilmasa stringni barchasini alohida array qilib beradi
+// let natija = str.split('')
+
+// agar split('harf') harf kiritilsa usha harflarni olib tashlab beradi 
+// let natija = str.split('a')
+
+
+
+console.log(natija);
+
+// const arr = [[false,true] , [1,2], 99 , "str"]
+// for (let i=0 ; i<arr.length ; i++){
+//     if  (typeof arr[i]=="string") {
+//         console.log(i);
+//     }
+// }
+
+
+
