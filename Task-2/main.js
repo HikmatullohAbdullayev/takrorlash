3f0cf981986122f86d05581efc7ff943a93a2756
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-13 14:36:28 +0500

    element qo'shish va o'ib tashlash

---
+++ Task-2/main.js
@@ -0,0 +1,25 @@
+// let ul = document.querySelector('.lists')
+// // ul.remove()
+
+
+// let btn = document.querySelector('.btn')
+// btn.addEventListener('click' , () => {
+//     ul.innerHTML += `<li class ="item">item</li>`
+    
+// })
+
+// let item = document.querySelectorAll('.item')
+// item.forEach((item) => {
+//     item.addEventListener('click' , (elem) => {
+//         elem.target.remove()
+//     })
+    
+// });
+
+
+
+
+
+
+
+
