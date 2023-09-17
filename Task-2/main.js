b13efb23d7aba3d49dcf3e53c67dd0bd3ccf842c
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-17 14:38:29 +0500

    arr

--- Task-2/main.js
+++ Task-2/main.js
@@ -18,7 +18,7 @@
 
 
 
-let arr1= [1,2,3,4,5,6]
+let arr1= [1,3,2,3,4,5,6]
 let arr2 = [6,5,4,3,2,1]
 
 // console.log(arr1.join(" "));
@@ -28,9 +28,40 @@ let arr2 = [6,5,4,3,2,1]
 // console.log(Array.isArray(arr1)); //true
 // console.log(Array.isArray("arr1")); //false
 
+// console.log(arr1);
+// arr1.push(2,3)
+// console.log(arr1);
+
+// arr1.pop()
+// console.log(arr1);
 
 
+// 0 dan indexgacha olib tashlaydi
+// arr1.splice(0,2)
 
+// kesib olib o'rniga qo'yadi
+// arr1.splice(0,2,"salom")
 
+// indexdan n ta sini olib tashlaydi
+// arr1.splice(2,1)
+
+// kiritilgan son ta  chiqazadi
+// arr1.splice(3)
+// arr1.splice(2)
+
+// kiritilgan maluorni index raqamini chiqazadi boshiqan qidiradi
+// console.log(arr1.indexOf(4));
+
+// oxiridan boshlab qidiradi
+// console.log(arr1.lastIndexOf(3));
+
+// teskari qilish
+// console.log(arr1.reverse());
+
+// kiritilgan qiymat bor yokida yo'q ekanligini tekshiradi //true //false
+// console.log(arr1.includes(22)); //false
+// console.log(arr1.includes(2)); //true
 
  
+
+// console.log(arr1.find(number => number ==2));
